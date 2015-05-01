d2d2cec5796eba69804d859645bd48937cc04430
Janos Potecki <potecki@cs.tum.edu>
2015-05-01 11:52:09 +0200

    add func listStringCompare

--- ConfigurationParser/ConfigParser.cpp
+++ ConfigurationParser/ConfigParser.cpp
@@ -1,36 +1,51 @@
 #include "ConfigParser.h"
 #include <string>
 #include <fstream>
+#include <vector>
+
+std::vector<std::string> s = {"spinning", "spinSpeed", "backgroundColor", "terrainPath", "terrainWidth", "terrainDepth", "terrainHeight"};
+
 
 ConfigParser::ConfigParser()
 {
+	
+	
+
 }
 
 
 ConfigParser::~ConfigParser()
 {
+
+
 }
 
 
 bool ConfigParser::listStringCompare(std::string str){
-	return true;
+	return (std::find(s.begin(), s.end(), str) != s.end());
 }
 
 
 void ConfigParser::load(std::string str){
-	
-		std::ifstream ifs(str);
-		// check if file is open
-		if (ifs.is_open()){
-			std::cout << "file is open";
+	int skip = 0;
+	// stream to read file
+	std::ifstream ifs(str);
 
-			// read words and print them in the console
-			std::string word;
-			while (ifs >> word){
-				std::cout << word << std::endl;
+	// check if file is open
+	if (ifs.is_open()){
+		std::cout << "file is open" << std::endl;
+
+		// read words and print them in the console
+		std::string word;
+
+		// read every word in the file
+		while (ifs >> word){
+			if (listStringCompare(word))
+			{
 			
 			}
+			
+			
 		}
-
-	
+	}	
 }
